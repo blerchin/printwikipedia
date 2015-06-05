b55055730ce1482c5130576f1e7a92de5e46fea9
Ben Lerchin <ben@lerchin.com>
2015-06-04 17:08:50 -0700

    Hide mobile dropdown nav after a link is clicked

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 $(function(){
-	toc_scroll = new Scroller($('#toc'), {
-		dataPath: function(num){
-			return "books/books_toc_web_" + num + ".json";
-		},
-		template: "book",
-		itemWidth: g.c.ITEM_WIDTH
-	});
-	articles_scroll = new Scroller($('#articles'), {
-		dataPath: function(num){
-			return "books/books_full_web_" + num + ".json";
-		},
-		template: "book",
-		itemWidth: g.c.ITEM_WIDTH
+	$('.navbar-collapse li a').click(function(){
+		$('.navbar-collapse').collapse('hide');
 	});
 });
